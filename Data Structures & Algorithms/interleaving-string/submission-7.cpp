d548e2c07d0da2b11cdd72b3a012031ba511f07b
siddharth reddy <114095055+itsbsiddharth@users.noreply.github.com>
2026-08-22 23:37:47 +0530

    Add: interleaving-string - submission-7

---
+++ Data Structures & Algorithms/interleaving-string/submission-7.cpp
@@ -0,0 +1,27 @@
+class Solution {
+   public:
+
+    bool isInterleave(string s1, string s2, string s3) {
+        int n1=s1.size(),n2=s2.size(),n3=s3.size();
+        if(n1+n2!=n3) return false;
+
+        vector<bool>dp(n2+1,false);
+
+        for(int i=n1;i>=0;i--){
+            for(int j=n2;j>=0;j--){
+                if(i==n1 && j==n2 ) {dp[j]=true;continue;}
+                               
+                bool f=false;
+
+                if(i<n1 && s1[i]==s3[i+j]) f= dp[j];
+                if(j<n2 && s2[j]==s3[i+j]) f= f || dp[j+1]; 
+
+                dp[j]=f;
+            }
+        }
+
+        return dp[0];
+    }   
+};
+
+//-1 not vis , 1 true, 0 false
